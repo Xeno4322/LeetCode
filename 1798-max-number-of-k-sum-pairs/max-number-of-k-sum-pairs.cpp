0bf4b55f60466d1648aeb8347b34cfba8bee02d5
Louis <114813860+Xeno4322@users.noreply.github.com>
2026-05-06 12:28:15 +0700

    Time: 115 ms (27.57%) | Memory: 76.7 MB (16.01%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int count = 0;
+
+        for (int num : nums){
+            int target = k - num;
+
+
+            if (freq[target] > 0){
+                count++;
+                freq[target]--;
+            }
+            else{
+                freq[num]++;
+            }
+        }
+
+        return count;
+    }
+};
